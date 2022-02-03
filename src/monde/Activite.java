de6e1509924210d5a6c8cf566a771864f34cf120
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-02-03 11:11:56 +0100

    Completion Activite et Guichet

--- src/monde/Activite.java
+++ src/monde/Activite.java
@@ -1,4 +1,24 @@
 package monde;
 
-public class Activite {
+import java.rmi.activation.ActivationID;
+
+public class Activite extends Etape {
+
+    private int temps;
+    private int ecartTemps;
+
+    Activite(String nom) {
+        super(nom);
+    }
+
+    Activite(String nom, int t, int e){
+        super(nom);
+        temps = t;
+        ecartTemps = e;
+    }
+
+    @Override
+    public boolean estUneActivite() {
+        return true;
+    }
 }
--- src/monde/Guichet.java
+++ src/monde/Guichet.java
@@ -1,4 +1,20 @@
 package monde;
 
-public class Guichet {
+public class Guichet extends Etape{
+
+    private int nbjetons;
+
+    Guichet(String nom){
+        super(nom);
+    }
+
+    Guichet(String nom, int nb){
+        super(nom);
+        nbjetons = nb;
+    }
+
+    @Override
+    public boolean estUnGuichet() {
+        return true;
+    }
 }
