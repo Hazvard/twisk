4e54ab904c40b95ef869c32e1d8e1378b6a82fcc
demang179u <DA77de37&*>
2022-02-07 19:53:46 +0100

    Début testMonde

---
+++ src/test/monde/MondeTest.java
@@ -0,0 +1,62 @@
+package test.monde;
+
+import Twisk.monde.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MondeTest {
+
+    static private Monde monde ;
+    static private Etape sasSortie ;
+    static private Etape sasEntree ;
+    static private ActiviteRestreinte actRestreinte ;
+    static private Guichet guichet ;
+    static private Activite activite ;
+
+    @BeforeEach
+    void setUp(){
+        monde = new Monde() ;
+        sasEntree = new SasEntree() ;
+        sasSortie = new SasSortie() ;
+        actRestreinte = new ActiviteRestreinte("ActResrt");
+        guichet = new Guichet("Gch");
+
+    }
+
+    @Test
+    void TestACommeEntree(){
+
+    }
+
+    @Test
+    void TestACommeSortie(){
+
+    }
+
+    @Test
+    void Testajouter(){
+        monde.ajouter(activite, actRestreinte, guichet);
+    }
+
+    @Test
+    void TestnbEtapes(){
+        monde.ajouter(activite, actRestreinte, guichet);
+
+
+    }
+
+    @Test
+    void TestNbGuichets(){
+
+    }
+
+    @Test
+    void hasNext(){
+
+    }
+}
