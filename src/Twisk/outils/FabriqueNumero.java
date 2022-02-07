b47f08418d749d1cd572b66323d30b6b50f3abd9
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-02-07 19:52:15 +0100

    Creation de FabriqueNumero et du package outils

---
+++ src/Twisk/outils/FabriqueNumero.java
@@ -0,0 +1,19 @@
+package Twisk.outils;
+//Comme FabriqueCarte
+public class FabriqueNumero {
+    private int cptEtape;
+
+    private FabriqueNumero(){
+    }
+
+    static FabriqueNumero getInstance(){
+        return null;
+    }
+
+    int getNumeroEtape(){
+        return 0;
+    }
+
+    void reset(){
+    }
+}
