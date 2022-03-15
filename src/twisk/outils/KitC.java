3b0a45f7fc6a761ce7ba09a9db9ee513d12643e0
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-03-15 15:22:38 +0100

    Séance 5 Q4 complete

--- src/twisk/outils/KitC.java
+++ src/twisk/outils/KitC.java
@@ -1,5 +1,7 @@
 package twisk.outils;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,11 +11,11 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class KitC {
 
-    public KitC(){
+    public KitC() {
 
     }
 
-    public void creerEnvironnement(){
+    public void creerEnvironnement() {
         try {
 // création du répertoire twisk sous /tmp. Ne déclenche pas d’erreur si le répertoire existe déjà
             Path directories = Files.createDirectories(Paths.get("/tmp/twisk"));
@@ -29,9 +31,26 @@ public class KitC {
         }
     }
 
-    void creerFichier(String codeC){
-
-
-
+    void creerFichier(String codeC) throws IOException {
+        File fichier = new File("tmp/twisk/client.c");
+        try {
+            if (fichier.createNewFile()) {
+                System.out.println("Fichier créé : " + fichier.getName());
+            } else {
+                System.out.println("Impossible de créé le fichier");
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur dans la lecture.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter texte = new FileWriter("tmp/twisk/client.c");
+            texte.write(codeC);
+            texte.close();
+            System.out.println("Ecrit dans client.c completed !");
+        } catch (IOException e) {
+            System.out.println("Erreur dans l'écriture");
+            e.printStackTrace();
+        }
     }
 }
