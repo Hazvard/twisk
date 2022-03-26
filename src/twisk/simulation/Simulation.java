84fb8d817ba0e9449002758335300d07179b7036
dan <dan.demang179u@etu.univ-lorraine.fr>
2022-03-26 09:57:42 -0700

    Création des fonctions native

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -11,6 +11,10 @@ public class Simulation{
 
     }
 
+    public native void start_simulation(int nbEtapes, int nbGuichets, int nbClients, int[] tabJetonsGuichet);
+    public native int[] ou_sont_les_clients(int nbEtapes, int nbClients);
+    public native void nettoyage();
+
     public void simuler(Monde world){
 
         //Les activités
