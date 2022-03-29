d68170913dda64770d445a604287c3ed0c49a60a
dan <dan.demang179u@etu.univ-lorraine.fr>
2022-03-29 05:35:13 -0700

    Client achevé et testé

--- src/twisk/simulation/Client.java
+++ src/twisk/simulation/Client.java
@@ -7,19 +7,48 @@ public class Client {
 
     private int numClient;
     private int rang;
-    private Monde monde;
+    private Etape etape;
 
 
     public Client(int numero){
         numClient = numero;
         rang = 0; // Pas certain de la validité de ça
+        etape = null;
     }
 
     public void allerA(Etape etape, int rang){
         this.rang = rang;
+        this.etape = etape;
 
 
     }
 
 
+
+    // Getteurs
+    public int getNumClient() {
+        return numClient;
+    }
+
+    public void setNumClient(int numClient) {
+        this.numClient = numClient;
+    }
+
+    public int getRang() {
+        return rang;
+    }
+
+
+    // Setteurs
+    public void setRang(int rang) {
+        this.rang = rang;
+    }
+
+    public Etape getEtape() {
+        return etape;
+    }
+
+    public void setEtape(Etape etape) {
+        this.etape = etape;
+    }
 }
