76c5c20a43d3dff495c4479679ab0df13691e8f5
dan <dan.demang179u@etu.univ-lorraine.fr>
2022-04-05 03:48:39 -0700

    début classloader

---
+++ src/twisk/outils/ClassLoaderPerso.java
@@ -0,0 +1,13 @@
+package twisk.outils;
+
+public class ClassLoaderPerso extends ClassLoader {
+
+    public ClassLoaderPerso(ClassLoader parent){
+
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name);
+    }
+}
