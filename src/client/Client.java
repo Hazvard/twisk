825176e8da3b1bcb5821500220bac66ee13e2397
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-04-26 11:34:06 +0200

    2 monde dans client(twisk)

--- src/client/Client.java
+++ src/client/Client.java
@@ -39,6 +39,20 @@ public class Client {
         //Simulation sim = new Simulation();
         //sim.simuler(world);
 
+        //Monde monde2 = new Monde();
+        //Acti
+        //Etape act11 = new Activite("db du prc", 5,3);
+        //Etape guiche = new Guichet("on est la", 2);
+        //Etape actiRest = new ActiviteRestreinte("RestMan", 5, 2);
+        //Etape act22 = new Activite("Fin du parc",5,3);
+        //Etape act33 = new Activite("Fin du parc 2", 5,3);
+        //act11.ajouterSuccesseur(guiche);
+        //guiche.ajouterSuccesseur(actiRest);
+        //actiRest.ajouterSuccesseur(act22);
+        //act22.ajouterSuccesseur(act33);
+        //monde2.aCommeSortie(act22);
+        //monde2.ajouter(act11,guiche,actiRest,act22);
+
 
         try {
             Client leClient = new Client();
@@ -46,10 +60,12 @@ public class Client {
             Class<?> laClasse = classLoaderPerso.loadClass("twisk.simulation.Simulation");
             Constructor<?> leConstructeur = laClasse.getConstructor();
             Object laSimulation = leConstructeur.newInstance();
-            Method setNbClients = laClasse.getMethod("setNbClient");
-            Method simulation = laClasse.getMethod("simuler");
+            Method setNbClients = laClasse.getMethod("setNbClients",int.class);
+            Method simulation = laClasse.getMethod("simuler", Monde.class);
             setNbClients.invoke(laSimulation, 4);
             simulation.invoke(laSimulation, world);
+
+            //simulation.invoke(laSimulation, monde2);
 
 
         }catch(ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e){
